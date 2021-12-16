401e2ad61e9dab53debe1036342601917eaba9d1
Victor Maya <victor.maya42@gmail.com>
2021-12-16 10:33:56 -0300

    useForm

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -2,14 +2,22 @@ import React from 'react';
 import styles from './Input.module.css';
 // import { Container } from './styles';
 
-function Input({ label, type, name }) {
+function Input({ label, type, name, value, onChange, error, onBlur }) {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input className={styles.input} id={name} name={name} type={type} />
-      <p className={styles.error}>Error</p>
+      <input
+        className={styles.input}
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 }
