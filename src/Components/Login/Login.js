62408db8281ce24248d8c865aab8b8d20859edf1
Victor Maya <victor.maya42@gmail.com>
2021-12-17 09:54:44 -0300

    validate token

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
+import { UserContext } from '../../UserContext';
 
 // import { Container } from './styles';
 
 function Login() {
-  return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='/criar' element={<LoginCreate />} />
-        <Route path='/perdeu' element={<LoginPasswordLost />} />
-        <Route path='/resetar' element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
-  );
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to='/conta' />
+    return (
+      <div>
+        <Routes>
+          <Route path='/' element={<LoginForm />} />
+          <Route path='/criar' element={<LoginCreate />} />
+          <Route path='/perdeu' element={<LoginPasswordLost />} />
+          <Route path='/resetar' element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    );
 }
 
 export default Login;
